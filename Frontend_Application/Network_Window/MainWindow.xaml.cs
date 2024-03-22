ead80e828d1bca98aae52268782b49e4b8392415
youssef youssef <youssef.youssef@kardex.com>
2024-03-22 16:20:19 +0100

    Isnt working, table is now accessible and checkboxes arent working

--- Frontend_Application/Network_Window/MainWindow.xaml.cs
+++ Frontend_Application/Network_Window/MainWindow.xaml.cs
@@ -31,7 +31,7 @@ namespace Network_Window
         private string impGateway;
         private string impIndex;
         private string pattern = @"^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$";
-        Dictionary<int,Tuple<string,string,string,string>> Added_Routes = new Dictionary<int, Tuple<string, string, string, string>>
+        Dictionary<int, Tuple<string, string, string, string>> Added_Routes = new Dictionary<int, Tuple<string, string, string, string>>
             {
             { 1, Tuple.Create("", "", "", "") },
             { 2, Tuple.Create("", "", "", "") },
@@ -44,7 +44,10 @@ namespace Network_Window
 
         int counter = 1;
         public string[] interfaces;
-        
+        Boolean Maschinennetz = false;
+        Boolean Internet = false;
+        int Current_Network = 0;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -64,7 +67,7 @@ namespace Network_Window
             impMask = Mask_Block.Text;
             impGateway = Gate_Block.Text;
             impIndex = Index_Block.Text;
-            
+
 
 
             if (!Regex.IsMatch(impIP, pattern))
@@ -79,7 +82,7 @@ namespace Network_Window
             {
                 output.Text = "Gateway ist ungültig";
             }
-            else if (counter==5)
+            else if (counter == 5)
             {
                 output.Text = "Maximale Anzahl an Routen erreicht, bitte löschen sie Routen.";
             }
@@ -90,7 +93,7 @@ namespace Network_Window
                 Gate_Block.Clear();
                 Index_Block.Clear();
 
-                
+
 
 
                 Process route_delete = new Process();
@@ -119,8 +122,8 @@ namespace Network_Window
                 string error_route_delete = route_delete.StandardError.ReadToEnd();
 
                 route_delete.WaitForExit();
-                
-                
+
+
                 if (!string.IsNullOrWhiteSpace(error_route_delete))
                 {
                     output.Text = $"Error: {error_route_delete}";
@@ -165,7 +168,7 @@ namespace Network_Window
                 }
                 else
                 {
-                    Added_Routes[counter]=Tuple.Create(impIP, impMask, impGateway, impIndex);
+                    Added_Routes[counter] = Tuple.Create(impIP, impMask, impGateway, impIndex);
                     output.Text = output_route_add;
 
                     string text = $"IP Adresse: {Added_Routes[counter].Item1}\nSubnetMaske: {Added_Routes[counter].Item2}\nGateway: {Added_Routes[counter].Item3}\nSchnittstellenindex: {Added_Routes[counter].Item4}\n";
@@ -189,14 +192,14 @@ namespace Network_Window
                 }
             }
         }
-        public ObservableCollection<string> ButtonItems { get; set; }
         private void Netzwerk_Button(object sender, RoutedEventArgs e)
         {
+            GridContainer.Children.Clear();
             string command = @"
                     Get-NetAdapter | ForEach-Object {
                     $Interface = $_
                     $IPConfiguration = Get-NetIPConfiguration -InterfaceIndex $Interface.InterfaceIndex
-                    $DNS = ($IPConfiguration.DNSServer.ServerAddresses | Where-Object { $_ -like '*.*.*.*' })
+                    $DNS = ($IPConfiguration.DNSServer.ServerAddresses | Where-Object { $_ -like '*.*.*.*' }) -join ','
                     if (-not $DNS) {
                         $DNS = 'NaN'
                     }
@@ -208,21 +211,21 @@ namespace Network_Window
                     }
                     $SubnetMaskDottedDecimal = ([ipaddress]$SubnetMaskDottedDecimal).IPAddressToString
 
-                    $InterfaceAlias = $Interface.InterfaceAlias.Replace(' ', '_')
+                    $InterfaceAlias = $Interface.InterfaceAlias -replace ' ', '_'
 
                     [PSCustomObject]@{
                         Index = $Interface.InterfaceIndex
                         InterfaceAlias = $InterfaceAlias
-                        Status = $Interface.Status
-                        IPAddress = $IPConfiguration.IPv4Address.IPAddress
-                        SubnetMask = $SubnetMaskDottedDecimal
-                        Gateway = $IPConfiguration.IPv4DefaultGateway.NextHop
+                        Status = if ($Interface.Status) { $Interface.Status } else { 'NaN' }
+                        IPAddress = if ($IPConfiguration.IPv4Address.IPAddress) { $IPConfiguration.IPv4Address.IPAddress } else { 'NaN' }
+                        SubnetMask = if ($SubnetMaskDottedDecimal) { $SubnetMaskDottedDecimal } else { 'NaN' }
+                        Gateway = if ($IPConfiguration.IPv4DefaultGateway.NextHop) { $IPConfiguration.IPv4DefaultGateway.NextHop } else { 'NaN' }
                         DNS = $DNS
                     }
                 } | Format-Table -AutoSize | Out-String -Width 4096";
 
             string output = ShellCommand(command);
-            
+
             //Unterteilt die Ausgabe in Zeilen
             string[] rows = output.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
             List<string[]> rowVariables = new List<string[]>();
@@ -234,71 +237,113 @@ namespace Network_Window
                 rowVariables.Add(columns);
             }
 
-            string test=null;
-            
-            
-            
-            
-            
-            ButtonItems = new ObservableCollection<string>();
-            
-            
-            
-            
-            
-            
-            string index="";
-            string ip="";
+            string test = null;
+
+            int count = rowVariables.Count - 2;
+
+
+
+
+
+            //ButtonItems = new ObservableCollection<string>();
+
+
+            int x = 0; // Column index
+            int v = 0;
+
+
+            string column_value = null;
+            string index = "";
+            string ip = "";
             string subnetMask = "";
-            string gateway="";
-            for (int j=2; j< rowVariables.Count; j++) {
+            string gateway = "";
+
+            for (int j = 2; j < rowVariables.Count; j++)
+            {
                 int runner = j - 1;
                 string[] testRow = rowVariables[j];
-                
-                
 
-                for (int i=0; i < testRow.Length; i++)
+
+
+                for (int i = 0; i < testRow.Length; i++)
                 {
                     test += $"{testRow[i]} ";
-                    
+                    column_value = testRow[i];
                     index = testRow[0];
                     ip = testRow[3];
                     subnetMask = testRow[4];
                     gateway = testRow[5];
+                    CreateRows(column_value, v,x);
+                    x++;
+                    if (x >= 7)
+                    {
+                        CheckBox checkBox = new CheckBox();
+                        checkBox.Content = "Hinzufügen";
+                        checkBox.FontSize = 12;
+                        checkBox.FontFamily = new System.Windows.Media.FontFamily("Consolas");
+                        checkBox.Foreground = new System.Windows.Media.SolidColorBrush(System.Windows.Media.Colors.White);
+                        checkBox.Checked += (sender, e) => CheckBox_Checked(runner);
+                        Grid.SetRow(checkBox, v);
+                        Grid.SetColumn(checkBox, x+1);
+                        GridContainer.Children.Add(checkBox);
+                        x = 0;
+                        v++;
+
+                    }
                     
+
                 }
-                
+
                 Tuple<string, string, string, string> value = Tuple.Create($"{ip}", $"{subnetMask}", $"{gateway}", $"{index}");
                 Netzwerke[runner] = value;
-                ButtonItems.Add(test);
+                //ButtonItems.Add(test);
                 test = null;
             }
-            
+
             //ButtonContainer.ItemsSource = ButtonItems;
 
         }
-        private void Button_Click(object sender, RoutedEventArgs e)
+
+        private void CheckBox_Checked(int value)
         {
-            // Handle the button click action here
-            Button button = (Button)sender;
-            string clickedItem = button.Content.ToString();
-            MessageBox.Show($"You clicked: {clickedItem}");
+            MessageBox.Show($"{Netzwerke[value].Item1} mask {Netzwerke[value].Item2} {Netzwerke[value].Item3} if {Netzwerke[value].Item4}");
+            Current_Network = value;
+
         }
+        private void CreateRows(string value, int row, int column)
+        {
+            int x = row;
+            int y= column;
+            TextBlock textBlock = new TextBlock();
+            textBlock.Text = $"{value}";
+            textBlock.FontSize = 12;
+            textBlock.TextWrapping = TextWrapping.Wrap;
+            textBlock.Width = 100;
+            textBlock.FontFamily = new System.Windows.Media.FontFamily("Consolas");
+            textBlock.Foreground = new System.Windows.Media.SolidColorBrush(System.Windows.Media.Colors.White);
+            textBlock.HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
+
+            Grid.SetRow(textBlock, x);
+            Grid.SetColumn(textBlock, y);
+            GridContainer.Children.Add(textBlock);
+        }
+
+
         private void Route_1_Delete_Click(object sender, RoutedEventArgs e)
         {
-            deleteRoute(1,Route_1.Text);
+            deleteRoute(1, Route_1.Text);
         }
         private void Route_2_Delete_Click(object sender, RoutedEventArgs e)
         {
-            deleteRoute(2,Route_2.Text);
+            deleteRoute(2, Route_2.Text);
         }
         private void Route_3_Delete_Click(object sender, RoutedEventArgs e)
         {
-            deleteRoute(3,Route_3.Text);
+            deleteRoute(3, Route_3.Text);
         }
         private void Route_4_Delete_Click(object sender, RoutedEventArgs e)
         {
-            deleteRoute(4,Route_4.Text);
+            deleteRoute(4, Route_4.Text);
         }
         private void deleteRoute(int index, string boxnr)
         {
@@ -367,7 +412,7 @@ namespace Network_Window
             }
         }
 
-        
+
         private void Routen_Button(object sender, RoutedEventArgs e)
         {
             Process process = new Process();
@@ -456,6 +501,132 @@ namespace Network_Window
             {
                 string output = response;
                 return output;
+            }
+        }
+
+        private void Maschinen_netz_box_Click(object sender, RoutedEventArgs e)
+        {
+            Maschinennetz = !Maschinennetz;
+            if (Maschinennetz)
+            {
+                Internet_box.IsEnabled = false;
+                Internet = false;
+            }
+            else
+            {
+                Internet_box.IsEnabled = true;
+            }
+        }
+
+        private void Internet_box_Click(object sender, RoutedEventArgs e)
+        {
+            Internet = !Internet;
+            if (Internet)
+            {
+                Maschinen_netz_box.IsEnabled = false;
+                Maschinennetz = false;
+                Route_Add_Automatically(Current_Network);
+            }
+            else
+            {
+                Maschinen_netz_box.IsEnabled = true;
+            }
+        }
+
+        private void Route_Add_Automatically(int value)
+        {
+            Process route_delete = new Process();
+
+            string command_route_delete = ("route delete 0.0.0.0 mask 0.0.0.0 " + Netzwerke[value].Item3);
+
+            // Set up the process start info
+            ProcessStartInfo startInfo_route_delete = new ProcessStartInfo
+            {
+                FileName = "powershell.exe",  // Specify PowerShell executable
+                Arguments = $"-NoProfile -ExecutionPolicy Bypass -Command \"{command_route_delete};route delete {Netzwerke[value].Item1} mask {Netzwerke[value].Item2} {Netzwerke[value].Item3}\"", // Pass the command as argument
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            route_delete.StartInfo = startInfo_route_delete;
+
+            // Start the process
+            route_delete.Start();
+
+            route_delete.WaitForExit();
+
+            string output_route_delete = route_delete.StandardOutput.ReadToEnd();
+            string error_route_delete = route_delete.StandardError.ReadToEnd();
+
+            route_delete.WaitForExit();
+
+
+            if (!string.IsNullOrWhiteSpace(error_route_delete))
+            {
+                output.Text = $"Error: {error_route_delete}";
+            }
+            else
+            {
+                output.Text = output_route_delete;
+            }
+
+            Process route_add = new Process();
+
+            string command_route_add = ("route add " + Netzwerke[value].Item1 + " mask " + Netzwerke[value].Item2 + " " + Netzwerke[value].Item3 + " if " + Netzwerke[value].Item4);
+
+            // Set up the process start info
+            ProcessStartInfo startInfo_route_add = new ProcessStartInfo
+            {
+                FileName = "powershell.exe",  // Specify PowerShell executable
+                Arguments = $"-NoProfile -ExecutionPolicy Bypass -Command \"{command_route_add}\"", // Pass the command as argument
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            route_add.StartInfo = startInfo_route_add;
+
+            // Start the process
+            route_add.Start();
+
+            route_add.WaitForExit();
+
+            string output_route_add = route_add.StandardOutput.ReadToEnd();
+            string error_route_add = route_add.StandardError.ReadToEnd();
+
+            route_add.WaitForExit();
+
+
+            if (!string.IsNullOrWhiteSpace(error_route_add))
+            {
+                output.Text = $"Error: {error_route_add}";
+            }
+            else
+            {
+                Added_Routes[counter] = Tuple.Create(Netzwerke[value].Item1, Netzwerke[value].Item2, Netzwerke[value].Item3, Netzwerke[value].Item4);
+                output.Text = output_route_add;
+
+                string text = $"IP Adresse: {Added_Routes[counter].Item1}\nSubnetMaske: {Added_Routes[counter].Item2}\nGateway: {Added_Routes[counter].Item3}\nSchnittstellenindex: {Added_Routes[counter].Item4}\n";
+
+                switch (counter)
+                {
+                    case 1:
+                        Route_1.Text = text;
+                        break;
+                    case 2:
+                        Route_2.Text = text;
+                        break;
+                    case 3:
+                        Route_3.Text = text;
+                        break;
+                    case 4:
+                        Route_4.Text = text;
+                        break;
+                }
+                counter++;
             }
         }
     }
