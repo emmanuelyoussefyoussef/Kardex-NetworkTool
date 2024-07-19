9f2a48233dfab0ab0b79aae8f6b7e1602d8b52c5
Emmanuel Youssef Youssef <you.youssef.23@lehre.mosbach.dhbw.de>
2024-07-19 15:10:50 +0200

    deleted exportes

--- Frontend_Application/Network_Window/MainWindow.xaml.cs
+++ Frontend_Application/Network_Window/MainWindow.xaml.cs
@@ -1,26 +1,9 @@
 ﻿using Network_Window.Info_button;
-using System;
-using System.Configuration;
 using System.Diagnostics;
-using System.DirectoryServices.ActiveDirectory;
-using System.Net;
-using System.Runtime.CompilerServices;
-using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Collections.Generic;
-using System.Data;
-using System.Reflection;
-using System.Collections.ObjectModel;
 
 namespace Network_Window
 {
