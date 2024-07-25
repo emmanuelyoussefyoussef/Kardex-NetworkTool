3f3d50abdc169bf4b98d3fc887f2f9bd4e70b7bf
Emmanuel Youssef Youssef <you.youssef.23@lehre.mosbach.dhbw.de>
2024-07-25 17:52:21 +0200

    changed the powershell command

--- Frontend_Application/Network_Window/terminalCommand.cs
+++ Frontend_Application/Network_Window/terminalCommand.cs
@@ -4,40 +4,46 @@ namespace Network_Window
 {
     public class TerminalCommand
     {
-        public string Command { get; set;}
-        public string Output { get; set;}
+        public string Command { get; set; }
+        public string Output { get; set; }
         public string Error { get; set; }
 
         private string GenerateNetwork = @"
                     Get-NetAdapter | ForEach-Object {
-                    $Interface = $_
-                    $IPConfiguration = Get-NetIPConfiguration -InterfaceIndex $Interface.InterfaceIndex
-                    $DNS = ($IPConfiguration.DNSServer.ServerAddresses | Where-Object { $_ -like '*.*.*.*' }) -join ','
-                    if (-not $DNS) {
-                        $DNS = 'EMPTY'
-                    }
-                    $SubnetMaskCIDR = $IPConfiguration.IPv4Address.PrefixLength
+    $Interface = $_
+    $IPConfiguration = Get-NetIPConfiguration -InterfaceIndex $Interface.InterfaceIndex
+    $DNS = ($IPConfiguration.DNSServer.ServerAddresses | Where-Object { $_ -like '*.*.*.*' }) -join ','
+    if (-not $DNS) {
+        $DNS = 'EMPTY'
+    }
+    $SubnetMaskCIDR = $IPConfiguration.IPv4Address.PrefixLength
 
-                    $SubnetMaskDottedDecimal = (([math]::pow(2, $SubnetMaskCIDR) - 1) -shl (32 - $SubnetMaskCIDR)) -band 0xFFFFFFFF
-                    if ($SubnetMaskDottedDecimal -lt 0) {
-                        $SubnetMaskDottedDecimal += [math]::pow(2, 32)
-                    }
-                    $SubnetMaskDottedDecimal = ([ipaddress]$SubnetMaskDottedDecimal).IPAddressToString
+    $SubnetMaskDottedDecimal = (([math]::pow(2, $SubnetMaskCIDR) - 1) -shl (32 - $SubnetMaskCIDR)) -band 0xFFFFFFFF
+    if ($SubnetMaskDottedDecimal -lt 0) {
+        $SubnetMaskDottedDecimal += [math]::pow(2, 32)
+    }
+    $SubnetMaskDottedDecimal = ([ipaddress]$SubnetMaskDottedDecimal).IPAddressToString
 
-                    $InterfaceAlias = $Interface.InterfaceAlias -replace ' ', '_'
+    $InterfaceAlias = $Interface.InterfaceAlias -replace ' ', '_'
+    $DNS = $DNS -replace ' ', '_'
+    $Gateway = if ($IPConfiguration.IPv4DefaultGateway.NextHop) { $IPConfiguration.IPv4DefaultGateway.NextHop -replace ' ', '_' } else { 'EMPTY' }
+    $IPAddress = if ($IPConfiguration.IPv4Address.IPAddress) { $IPConfiguration.IPv4Address.IPAddress -replace ' ', '_' } else { 'EMPTY' }
+    $SubnetMask = if ($SubnetMaskDottedDecimal) { $SubnetMaskDottedDecimal -replace ' ', '_' } else { 'EMPTY' }
+    $Status = if ($Interface.Status) { $Interface.Status -replace ' ', '_' } else { 'EMPTY' }
 
-                    [PSCustomObject]@{
-                        Index = $Interface.InterfaceIndex
-                        InterfaceAlias = $InterfaceAlias
-                        Status = if ($Interface.Status) { $Interface.Status } else { 'EMPTY' }
-                        IPAddress = if ($IPConfiguration.IPv4Address.IPAddress) { $IPConfiguration.IPv4Address.IPAddress } else { 'EMPTY' }
-                        SubnetMask = if ($SubnetMaskDottedDecimal) { $SubnetMaskDottedDecimal } else { 'EMPTY' }
-                        Gateway = if ($IPConfiguration.IPv4DefaultGateway.NextHop) { $IPConfiguration.IPv4DefaultGateway.NextHop } else { 'EMPTY' }
-                        DNS = $DNS
-                    }
-                } | Format-Table -AutoSize | Out-String -Width 4096";
-        public void CommandShell(string _Command) {
-            
+    [PSCustomObject]@{
+        Index = $Interface.InterfaceIndex
+        InterfaceAlias = $InterfaceAlias
+        Status = $Status
+        IPAddress = $IPAddress
+        SubnetMask = $SubnetMask
+        Gateway = $Gateway
+        DNS = $DNS
+    }
+} | Format-Table -AutoSize | Out-String -Width 4096";
+        public void CommandShell(string _Command)
+        {
+
             Process process = new Process();
 
             ProcessStartInfo processStart = new ProcessStartInfo
